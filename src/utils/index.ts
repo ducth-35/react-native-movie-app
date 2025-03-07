c98afb40f9d6fb6708eeed41d5f630c7a1e3752c
ducth <truonghuynhduc2k@gmail.com>
2025-03-07 09:47:08 +0700

    refactor code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,28 +1,3 @@
-export const isNullOrEmpty = (data: any | any[]): boolean => {
-  if (!data) {
-    return true;
-  }
-  if (data instanceof Array) {
-    return data.length === 0;
-  }
-  if (typeof data === 'number') {
-    return data === 0;
-  }
-  if (typeof data === 'undefined') {
-    return true;
-  }
-  if (typeof data === 'object') {
-    return Object.keys(data).length === 0;
-  }
-  let output = data;
-  if (typeof output !== 'string') {
-    output = output.toString();
-  }
-  output = output.trim();
-
-  return output.length <= 0;
-};
-
 export const shuffle = <T>(array: T[]): T[] => {
   let newArray = array.slice();
   let currentIndex = newArray.length;
